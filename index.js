ac5b1d0853106f6fd07269634ff59c3599dcf128
Ivan <vitanlhouse@gmail.com>
2020-10-25 21:21:23 +0200

    Переводить звичайні функції в методи класу

--- index.js
+++ index.js
@@ -9,6 +9,56 @@ class CountdownTimer {
   constructor({ refs, targetDate }) {
     this.refs = refs;
     this.targetDate = targetDate;
+
+    this.updateTimer(this.getTimeComponents());
+
+    setInterval(() => {
+      this.updateTimer(this.getTimeComponents());
+    }, 1000);
+  }
+
+  getTimeComponents() {
+    const time = this.targetDate - Date.now();
+
+    /*
+     * Дні, що залишилися: ділимо значення UTC на 1000 * 60 * 60 * 24, кількість
+     * мілісекунд в один день (мілісекунди * секунди * хвилини * години)
+     */
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    /*
+     * Решта годин: отримуємо залишок від попереднього розрахунку за допомогою оператора
+     * залишку% і ділимо його на кількість мілісекунд в одній годині
+     * (1000 * 60 * 60 = мілісекунди * хвилини * секунди)
+     */
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+
+    /*
+     * Решта хвилин: отримуємо хвилини, що залишилися і ділимо їх на кількість
+     * мілісекунд в одній хвилині (1000 * 60 = мілісекунди * секунди)
+     */
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+    /*
+     * Решта секунд: отримуємо секунди, які залишилися і ділимо їх на кількість
+     * миллисекунд в одной секунде (1000)
+     */
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  updateTimer({ days, hours, mins, secs }) {
+    this.refs.daysEl.textContent = days;
+    this.refs.hoursEl.textContent = hours;
+    this.refs.minsEl.textContent = mins;
+    this.refs.secsEl.textContent = secs;
   }
 }
 
@@ -16,53 +66,3 @@ const countdownTimer = new CountdownTimer({
   refs: refs,
   targetDate: new Date(2020, 12, 12, 12, 12, 12, 12),
 });
-
-updateTimer({ days: '00', hours: '00', mins: '00', secs: '00' });
-
-setInterval(() => {
-  updateTimer(getTimeComponents());
-}, 1000);
-
-function getTimeComponents() {
-  const time = countdownTimer.targetDate - Date.now();
-
-  /*
-   * Дні, що залишилися: ділимо значення UTC на 1000 * 60 * 60 * 24, кількість
-   * мілісекунд в один день (мілісекунди * секунди * хвилини * години)
-   */
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-  /*
-   * Решта годин: отримуємо залишок від попереднього розрахунку за допомогою оператора
-   * залишку% і ділимо його на кількість мілісекунд в одній годині
-   * (1000 * 60 * 60 = мілісекунди * хвилини * секунди)
-   */
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
-
-  /*
-   * Решта хвилин: отримуємо хвилини, що залишилися і ділимо їх на кількість
-   * мілісекунд в одній хвилині (1000 * 60 = мілісекунди * секунди)
-   */
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-
-  /*
-   * Решта секунд: отримуємо секунди, які залишилися і ділимо їх на кількість
-   * миллисекунд в одной секунде (1000)
-   */
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  return { days, hours, mins, secs };
-}
-
-function updateTimer({ days, hours, mins, secs }) {
-  refs.daysEl.textContent = days;
-  refs.hoursEl.textContent = hours;
-  refs.minsEl.textContent = mins;
-  refs.secsEl.textContent = secs;
-}
-
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
